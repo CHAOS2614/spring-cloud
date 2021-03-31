482537ab0c97a9feab782fbc50aa18a7da1a81b7
CHAOS2614 <330276680@qq.com>
2021-03-31 09:57:31 +0800

    服务注册中心eureka配置完成

---
+++ cloud2020/cloud-eureka-server7001/src/main/java/cn/edu/bjfu/springcloud/EurekaMain7001.java
@@ -0,0 +1,17 @@
+package cn.edu.bjfu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author Chaos
+ * @date 2021/3/31
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaMain7001 {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaMain7001.class, args);
+    }
+}
