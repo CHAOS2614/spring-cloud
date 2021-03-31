de609b4344d659ea7f040d7b8418544dca0d1e6b
CHAOS2614 <330276680@qq.com>
2021-03-31 10:07:57 +0800

    cloud-payment-service注册进eureka

--- cloud2020/cloud-provider-payment8001/src/main/java/cn/edu/bjfu/springcloud/PaymentMain8001.java
+++ cloud2020/cloud-provider-payment8001/src/main/java/cn/edu/bjfu/springcloud/PaymentMain8001.java
@@ -2,12 +2,14 @@ package cn.edu.bjfu.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author Chaos
  * @date 2021/3/29
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class PaymentMain8001 {
     public static void main(String[] args) {
         SpringApplication.run(PaymentMain8001.class, args);
