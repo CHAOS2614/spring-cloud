6a8d8720b7b84c4124829b9ac3df767fbad98996
CHAOS2614 <330276680@qq.com>
2021-04-09 09:52:51 +0800

    服务调用-OpenFeign

---
+++ cloud2020/cloud-feign-order80/src/main/java/cn/edu/bjfu/springcloud/service/PaymentFeignService.java
@@ -0,0 +1,21 @@
+package cn.edu.bjfu.springcloud.service;
+
+import cn.edu.bjfu.springcloud.entity.CommonResult;
+import cn.edu.bjfu.springcloud.entity.Payment;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+/**
+ * @author Chaos
+ * @date 2021/4/9
+ */
+@Component
+@FeignClient(value = "CLOUD-PAYMENT-SERVICE")
+public interface PaymentFeignService {
+
+    @GetMapping(value = "/payment/get/{id}")
+    CommonResult<Payment> getPaymentById(@PathVariable("id") Long id);
+
+}
