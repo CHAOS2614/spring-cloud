fb973eef00a346bddde14a960e98ba09124953ff
CHAOS2614 <330276680@qq.com>
2021-04-12 11:41:33 +0800

    gateway--自定义简单全局过滤器

---
+++ cloud2020/could-gatway-gatway9527/src/main/java/cn/edu/bjfu/springcloud/filter/MyLogGateWayFilter.java
@@ -0,0 +1,42 @@
+package cn.edu.bjfu.springcloud.filter;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import java.util.Date;
+
+/**
+ * @author Chaos
+ * @date 2021/4/12
+ */
+@Component
+@Slf4j
+public class MyLogGateWayFilter implements GlobalFilter, Ordered {
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        log.info(new Date() + "---------->全局日志过滤器！");
+        String uname = exchange.getRequest().getQueryParams().getFirst("uname");
+        if(uname == null){
+            log.info(new Date() + "---------->用户名不能为null！");
+            exchange.getResponse().setStatusCode(HttpStatus.NOT_ACCEPTABLE);
+            return exchange.getResponse().setComplete();
+        }
+        return chain.filter(exchange);
+    }
+
+    /**
+     * 过滤器优先级，越小越高
+     * @return 优先级
+     */
+    @Override
+    public int getOrder() {
+        return 0;
+    }
+}
