464cd6587ac3e6f19873d40c537d8e4044296d01
CHAOS2614 <330276680@qq.com>
2021-04-13 22:01:44 +0800

    cloud-stream-生产者发送消息

---
+++ cloud2020/cloud-stream-rabbitmq-provider8801/src/main/java/cn/edu/bjfu/springcloud/StreamMQMain8801.java
@@ -0,0 +1,15 @@
+package cn.edu.bjfu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Chaos
+ * @date 2021/4/13
+ */
+@SpringBootApplication
+public class StreamMQMain8801 {
+    public static void main(String[] args) {
+        SpringApplication.run(StreamMQMain8801.class, args);
+    }
+}
---
+++ cloud2020/cloud-stream-rabbitmq-provider8801/src/main/java/cn/edu/bjfu/springcloud/service/IMessageProvider.java
@@ -0,0 +1,9 @@
+package cn.edu.bjfu.springcloud.service;
+
+/**
+ * @author Chaos
+ * @date 2021/4/13
+ */
+public interface IMessageProvider {
+    String send();
+}
