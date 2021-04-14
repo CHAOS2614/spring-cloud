60c8ad4bac49a7305c0fd0af3a21ddcaea2e2a74
CHAOS2614 <330276680@qq.com>
2021-04-14 10:20:33 +0800

    cloud-stream-消费者接收消息

---
+++ cloud2020/cloud-stream-rabbitmq-consumer8802/src/main/java/cn/edu/bjfu/springcloud/service/impl/ReceiveMessageListener.java
@@ -0,0 +1,28 @@
+package cn.edu.bjfu.springcloud.service.impl;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.cloud.stream.messaging.Sink;
+import org.springframework.messaging.Message;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+/**
+ * @author Chaos
+ * @date 2021/4/14
+ */
+@Component
+@EnableBinding(Sink.class)
+public class ReceiveMessageListener {
+
+    @Value("${server.port}")
+    private String serverPort;
+
+    @StreamListener(Sink.INPUT)
+    public void input(Message<String> message) {
+        System.out.println(new Date() + "---------->消费者1号接收到的消息：" + message.getPayload() + "\t port: " + serverPort);
+    }
+
+}
